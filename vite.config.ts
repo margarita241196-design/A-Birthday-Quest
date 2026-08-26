6024161b6c916e6237475902991915805b90c4aa
Rita <margarita24.11.96@gmail.com>
2026-08-26 15:37:11 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/A-Birthday-Quest/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
